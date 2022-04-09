1f770cdda8897b3b708212ceca61f25398896404
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2022-04-09 01:25:15 +0100

    updates

--- C-Sharp/BookRegistry/Program.cs
+++ C-Sharp/BookRegistry/Program.cs
@@ -7,7 +7,7 @@ namespace BookRegistry
         static void Main(string[] args)
         {
             Book newbook1 = new Book("Jim's Story", "Jim Jimmy", 4503);
-            Book newbook2 = new Book("Jam's Yrots", "Jam Jammy", 3054);
+            Book newbook2 = new Book("Jim's Story", "Jim Jimmy", 4503);
 
 
             Console.WriteLine(newbook1.pages);
