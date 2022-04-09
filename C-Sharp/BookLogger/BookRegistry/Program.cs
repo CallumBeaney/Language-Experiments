7df39d6201604edcbfc12a41f7094108a60f2b73
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2022-04-09 01:22:08 +0100

    firstupload

---
+++ C-Sharp/BookLogger/BookRegistry/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BookRegistry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book newbook1 = new Book("Jim's Story", "Jim Jimmy", 4503);
+            Book newbook2 = new Book("Jam's Yrots", "Jam Jammy", 3054);
+
+
+            Console.WriteLine(newbook1.pages);
+
+            Console.ReadLine();
+        }
+    }
+}
